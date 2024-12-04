a8d56a4448058ca6afb07b9728c4d7bc9f5983cd
Mota-Jesus <a.22308051281275@cbtis128.edu.mx>
2024-12-03 20:13:37 -0600

    Add files via upload
    inventario_app y materia_app

---
+++ inventario_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Inventario(models.Model):
+    id_inventario = models.CharField(primary_key=True, max_length=6)
+    id_producto = models.IntegerField()
+    cantidad = models.IntegerField()
+    fecha_ingreso = models.DateField(null=False, blank=False)
+    fecha_vencimiento = models.DateField(null=False, blank=False)
+    proveedor = models.CharField(max_length=100)
+    Notas = models.TextField()
+
+    def __str__(self):
+        return f"Inventario {self.id_inventario} - Producto {self.id_producto}"
